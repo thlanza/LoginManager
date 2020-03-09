40c74e2d84ba3ef461ddbaaf11d1fd66b902add1
raphaelbarcala <59846296+raphaelbarcala@users.noreply.github.com>
2020-03-09 16:27:13 -0300

    Parte do user_manager

--- Adapters/user_manager.js
+++ Adapters/user_manager.js
@@ -4,21 +4,48 @@ class UserManager{
     
     constructor(){
         this.db = new user_db_manager();
+        
     }
 
     listUser(){
+        var list = this.user_db.listUser();
 
+        return list;
     }
 
     getUser(id){
-
+        if(!id || id ==''){
+            throw new Error('ID invalido')
+        }
+        try{
+            let user = this.user_db.getUserById(id);
+            return user;
+        }
+        catch (e) {
+            throw new Error(e);
+        }
     }
 
     addUser(){
 
+
     }
 
     deleteUser(id){
+        if (!id || id==""){
+            throw new Error("ID invalido")
+        }
+        else if(!this.user_db.getUserById(id)){
+            throw new Error("Servico nao encontrado");
+        }   
+
+    try {
+        this.user_db.removeUser(id);
+        }
+    catch (e) {
+        this.log.fail("Remover," + e);
+    }
+    this.log.success("Removido")
 
     }
 
