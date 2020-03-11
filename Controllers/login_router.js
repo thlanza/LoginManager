ea8fd6b486d052572571f47b183aa9724fabe858
Francisco Junio <francisco.junio@yahoo.com.br>
2020-03-11 11:41:54 -0300

    Revert "testando mongoose"
    This reverts commit 586d25c181787b10b91449be1006bdd9d61b0ca1.

--- Controllers/login_router.js
+++ Controllers/login_router.js
@@ -28,52 +28,68 @@ module.exports = (app) => {
     //     }
     // });
 
+    app.post('/Login/:hash', validator.postAPI, (req, res) =>{
+        var login = new login_manager();
+        try{
+            let success = login.validarLoginAPI(req.body, req.params.hash);
+            if(success === true){
+                return res.send('Sucesso.') //O Front deverá redirecionar o usuário para o serviço que o chamou
+            }
+            else{
+                return res.status(401).send();
+            }
+        }
+        catch(e){
+            return res.status(500).send(e);
+        }
+    });
+
     app.get("/", (req, resp) => {
         //NGINX entrega o front pro cliente
 
         resp.send(`
-        <html>
-        <body>
-        <center><br><br><br>
-        <h1> Auth Login </h1>
-        <form class="form" action="/login" method="post">
-        <div class="formulario">
-        <label for="Email">Email</label>
-        <input id="email" type="email" name="email" placeholder="insira o email" />
-        </div>
-        <div class="formulario">
-        <label for="senha">Senha</label>
-        <input id="senha" type="password" name="senha" placeholder="insira o senha"/>
-        </div>
-        <input type="submit" value="Enviar" />
-        </form>
-        </center>
-        </body> 
-        </html>
-        `);
+              <html>
+                  <body>
+                  <center><br><br><br>
+                  <h1> Auth Login </h1>
+                    <form class="form" action="/login" method="post">
+                      <div class="formulario">
+                        <label for="Email">Email</label>
+                        <input id="email" type="email" name="email" placeholder="insira o email" />
+                      </div>
+                      <div class="formulario">
+                        <label for="senha">Senha</label>
+                        <input id="senha" type="password" name="senha" placeholder="insira o senha"/>
+                      </div>
+                        <input type="submit" value="Enviar" />
+                    </form>
+                    </center>
+                  </body> 
+              </html>
+          `);
     });
 
     app.get("/adduser", (req, resp) => {
 
         resp.send(`
-        <html>
-        <body>
-        <center><br><br><br>
-        <h1> Auth AddUser teste </h1>
-        <form class="form" action="/adduser" method="post">
-        <div class="formulario">
-        <label for="email">Email</label>
-        <input id="email" type="email" name="email" placeholder="insira o login" />
-        </div>
-        <div class="formulario">
-        <label for="senha">Senha</label>
-        <input id="senha" type="password" name="senha" placeholder="insira o senha"/>
-        </div>
-        <input type="submit" value="Enviar" />
-        </form>
-        </center>
-        </body> 
-        </html>
+            <html>
+                <body>
+                <center><br><br><br>
+                <h1> Auth AddUser teste </h1>
+                <form class="form" action="/adduser" method="post">
+                    <div class="formulario">
+                    <label for="email">Email</label>
+                    <input id="email" type="email" name="email" placeholder="insira o login" />
+                    </div>
+                    <div class="formulario">
+                    <label for="senha">Senha</label>
+                    <input id="senha" type="password" name="senha" placeholder="insira o senha"/>
+                    </div>
+                    <input type="submit" value="Enviar" />
+                </form>
+                </center>
+                </body> 
+            </html>
         `);
     });
 
@@ -85,46 +101,30 @@ module.exports = (app) => {
 
     app.post('/login', validator.post, async (req, res) => {
 
-        const hash = await bcrypt.hash(req.body.senha, 10);
+        // const hash = await bcrypt.hash(req.body.senha, 10);
         const loginManager = new Login_manager();
         const msg = await loginManager.validarLogin(req.body.email, hash);
         return res.send(msg)
 
     });
 
-    app.post('/Login/:hash', validator.postAPI, (req, res) => {
-        var login = new login_manager();
-        try {
-            let success = login.validarLoginAPI(req.body, req.params.hash);
-            if (success === true) {
-                return res.send('Sucesso.') //O Front deverá redirecionar o usuário para o serviço que o chamou
-            }
-            else {
-                return res.status(401).send();
-            }
-        }
-        catch (e) {
-            return res.status(500).send(e);
-        }
-    });
-
     app.post('/adduser', validator.post, async (req, res) => {
 
-        const hash = await bcrypt.hash(req.body.senha, 10);
+        // const hash = await bcrypt.hash(req.body.senha, 10);
         // const loginManager = new Login_manager();
         // const msg = await loginManager.addUser(req.body.email, hash);
         // return res.send(msg);
 
         const _UserDataBaseManager = new UserDataBaseManager();
-        const msg = await _UserDataBaseManager.addUser(req.body.email, hash)
+        const msg = await _UserDataBaseManager.addUser(req.body.email, req.body.senha)
         return res.send(msg)
     });
 
 
-    // app.get("/listid", async (req, resp) => {
-    //     const _UserDataBaseManager = new UserDataBaseManager();
-    //     const msg = await _UserDataBaseManager.getUserById("5e68d9cf8f837e0de8781dce");
-    //     return resp.send(msg);
-    // });
+    app.get("/listid", async (req, resp) => {
+        const _UserDataBaseManager = new UserDataBaseManager();
+        const msg = await _UserDataBaseManager.getUserById("5e68d9cf8f837e0de8781dce");
+        return resp.send(msg);
+    });
 
 }
